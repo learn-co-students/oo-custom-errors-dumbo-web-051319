bfc5759ccd64e99fdcc24f697418115ea17b0d3a
Tanuka16 <das.tanuka@yahoo.com>
2019-05-19 21:41:11 -0400

    Done.

--- custom_errors.rb
+++ custom_errors.rb
@@ -10,14 +10,15 @@ class Person
     if person.class != Person
       #raise PartnerError
       begin
-        raise PartnerError
-      rescue PartnerError => error
-          puts error.message
+        raise PartnerError                #raise the error
+      rescue PartnerError => error        #rescus error;The rescue method creates an instance of the PartnerError class
+          puts error.message              #and puts out the result of calling message on that instance.
     else
       person.partner = self
     end
   end
 
+# Defining the custom error class
   class PartnerError < StandardError
     def message
     "you must give the get_married method an argument of an instance of the person class!"
